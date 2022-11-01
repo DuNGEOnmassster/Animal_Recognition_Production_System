f5484a3fe73cecb1a6aaa5dff1d1e65559d7bf0b
NormanZ <1275133384@qq.com>
2022-11-01 22:57:42 +0800

    update check extend text rules

--- utils/check_rules.py
+++ utils/check_rules.py
@@ -22,7 +22,6 @@ def check_extend_rules(txt_or_txt_file):
     if os.path.exists(txt_or_txt_file):
         file = open(txt_or_txt_file, 'r')
         readfile = file.read()
-        is_file = True
         for rule in readfile.split(sep="\n"):
             if "：" in rule:
                 rule_segment = rule.split(sep="：")
@@ -31,9 +30,28 @@ def check_extend_rules(txt_or_txt_file):
                         return False
                 else:
                     return False
+            else:
+                return False
+        print("Legal extend file rules")
+        return True
+    # Check the legitimacy of text rules
+    else:
+        if "：" in txt_or_txt_file:
+            rule_segment = txt_or_txt_file.split(sep="：")
+            if len(rule_segment) == 2:
+                if rule_segment[0].split(sep="）")[1] == None or rule_segment[1] == None:
+                    return False
+            else:
+                return False
+            print("Legal extend text rules")
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
     check_origin_rules()
     extend_path = "./dataset/extend_rules.txt"
+    extend_text = "（3）犀牛：有毛发，有奶，鼻子上有角，褐色，皮糙肉后，皮糙肉厚，有蹄；"
     check_extend_rules(extend_path)
+    check_extend_rules(extend_text)
